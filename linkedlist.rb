bc2982f2bb7df045adb51c5ac48eb58c5b48d531
Ben Hulan <benhulan@gmail.com>
2016-06-21 11:21:30 -0700

    refactor ruby solution

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,13 +1,3 @@
-class Node
-  attr_accessor :val, :next
-
-  def initialize(val, next_node)
-      @val = val
-      @next = next_node
-  end
-end
-
-
 class LinkedList
 
   def initialize(val)
---
+++ node.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :val, :next
+
+  def initialize(val, next_node)
+      @val = val
+      @next = next_node
+  end
+end
