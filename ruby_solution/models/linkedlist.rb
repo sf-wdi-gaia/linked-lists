b1c882f01d4b2b5c4d16446f511f457c3144a526
Ben Hulan <benhulan@gmail.com>
2016-06-22 12:50:35 -0700

    adds ruby challenges and comments

--- ruby_solution/models/linkedlist.rb
+++ ruby_solution/models/linkedlist.rb
@@ -2,11 +2,13 @@ class LinkedList
 
   attr_reader :head
   
+  # Instantiate a new LinkedList and assign a Node at the head.
   def initialize(val)
     @head = Node.new(val, nil)
   end
 
-  def add(val)
+  # Instantiate a new Node and add it as the tail of our LinkedList
+  def addAtEnd(val)
     current = @head
     while current.next != nil
       current = current.next
@@ -14,6 +16,54 @@ class LinkedList
     current.next = Node.new(val, nil)
   end
 
+  # Print the list to the terminal
+  def print_list
+    elements = []
+    current = @head
+    while current.next != nil
+      elements << current
+      current = current.next
+    end
+    elements << current
+    p elements
+  end
+
+  # Instantiate a new Node and add it as the head of our LinkedList
+  def insertAtHead(val)
+
+  end
+
+  # Traverse the list and return the number of Nodes found
+  def length
+
+  end
+
+  # Traverse the list and tell us if a certain value is found
+  def exists(val)
+
+  end
+
+  # Traverse the list and call a function on each Node
+  def each(f)
+
+  end
+
+  # Traverse the list and return the index of a Node with a given value
+  def indexOf(val)
+
+  end
+
+  # Traverse the list and return the value of a Node at a given index
+  def dataFrom(index)
+
+  end
+
+  # Traverse the list, create a Node with a specific value and insert it at a given index
+  def insertAt(index, val)
+
+  end
+
+  # Traverse the list, find the Node with the corresponding data and remove it without breaking the list.
   def delete(val)
     current.next = @head
     if current.val = val
@@ -28,13 +78,4 @@ class LinkedList
     end
   end
 
-  def return_list
-    elements = []
-    current = @head
-    while current.next != nil
-      elements << current
-      current = current.next
-    end
-    elements << current
-  end
 end
--- ruby_solution/models/node.rb
+++ ruby_solution/models/node.rb
@@ -1,6 +1,7 @@
 class Node
   attr_accessor :val, :next
 
+  # Make a new Node with two attributes:  val and next
   def initialize(val, next_node)
       @val = val
       @next = next_node
